ab609fb11efa2e34c1990d789fcd703dfe6fb392
Pedro Fonseca Esteves <muradean@gmail.com>
2020-05-10 20:20:55 +0100

    added schedule

--- src/Schedule_Getter.py
+++ src/Schedule_Getter.py
@@ -7,15 +7,15 @@ entry_lines["saidas"] = {}
 
 for cor in ["azul","amarela","verde","vermelha"]:
     for tipo in ["entradas","saidas"]:
-        path = "linha_" + cor + "_" + tipo + "1.ods"
+        path = "schedules/linha_" + cor + "_" + tipo + "1.ods"
         sheet_idx = 1
         df = read_ods(path, sheet_idx)
         entry_lines[tipo][cor] = df
         print("cor " + str(cor) + str(df.shape))
 
-path = "linha_vermelha_entradas1.ods"
-sheet_idx = 1
-df4 = read_ods(path, sheet_idx)
+#path = "linha_vermelha_entradas1.ods"
+#sheet_idx = 1
+#df4 = read_ods(path, sheet_idx)
 
 
 hours_dicio = {}
@@ -30,6 +30,7 @@ with open('horas.txt') as openfileobject:
 
 #tipo e um booleano que devolve entradas e saidas
 def extract_value(line,hour,station,tipo):
+
     
     if tipo == 1:#entradas
         line = entry_lines["entradas"][line]
@@ -40,10 +41,16 @@ def extract_value(line,hour,station,tipo):
         line = entry_lines["saidas"][line]
         index = hours_dicio[hour]
         res = line[station][index]
-        print(line.keys())
-    if(str(res) == "nan"):
-        return 0
-        
+
+    res = str(res)
+
+    try:
+        float(res)
+        if(res == "nan"):
+            return 0
+        return float(res)
+    except ValueError:
+        return 0        
     
 
-print(extract_value("azul",'06:30-45',"Alfornelos",0))
+print(extract_value("azul",'06:15-30',"Alfornelos",1))
