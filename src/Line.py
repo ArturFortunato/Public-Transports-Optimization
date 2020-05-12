2a9450f4d19aaf62873e8d9d513e6ea59f265ddd
ArturFortunato <artur.fortunato1@gmail.com>
2020-05-12 11:54:39 +0100

    red & green lines

--- src/Line.py
+++ src/Line.py
@@ -5,18 +5,18 @@ from Reporter import Reporter
 import re
 
 #verde
-telheiras = Station('Telheiras', 'Telheiras', 'Cais do Sodré', 0, [750, 650])
+telheiras = Station('Telheiras', 'Telheiras', 'Cais do Sodré', 0, [750, 600])
 campo_grande = Station('Campo Grande', 'Telheiras', 'Cais do Sodré', 8, None)
-alvalade = Station('Alvalade', 'Telheiras', 'Cais do Sodré', 20, None)
-roma = Station('Roma', 'Telheiras', 'Cais do Sodré', 26, None)
-areeiro = Station('Areeiro', 'Telheiras', 'Cais do Sodré', 33, None)
+alvalade = Station('Alvalade', 'Telheiras', 'Cais do Sodré', 20, [750, 450])
+roma = Station('Roma', 'Telheiras', 'Cais do Sodré', 26, [750, 400])
+areeiro = Station('Areeiro', 'Telheiras', 'Cais do Sodré', 33, [750, 350])
 alameda = Station('Alameda', 'Telheiras', 'Cais do Sodré', 40, [750, 300])
-arroios = Station('Arroios', 'Telheiras', 'Cais do Sodré', 44, None)
-anjos = Station('Anjos', 'Telheiras', 'Cais do Sodré', 48, None)
-intendente = Station('Intendente', 'Telheiras', 'Cais do Sodré', 53, None)
-martim_moniz = Station('Martim Moniz', 'Telheiras', 'Cais do Sodré', 60, None)
-rossio = Station('Rossio', 'Telheiras', 'Cais do Sodré', 65, None)
-baixa_chiado = Station('Baixa Chiado', 'Telheiras', 'Cais do Sodré', 71, [750, 105])
+arroios = Station('Arroios', 'Telheiras', 'Cais do Sodré', 44, [750, 260])
+anjos = Station('Anjos', 'Telheiras', 'Cais do Sodré', 48, [750, 220])
+intendente = Station('Intendente', 'Telheiras', 'Cais do Sodré', 53, [750, 180])
+martim_moniz = Station('Martim Moniz', 'Telheiras', 'Cais do Sodré', 60, [750, 150])
+rossio = Station('Rossio', 'Telheiras', 'Cais do Sodré', 65, [750, 120])
+baixa_chiado = Station('Baixa Chiado', 'Telheiras', 'Cais do Sodré', 71, [750, 80])
 cais_do_sodre = Station('Cais do Sodré', 'Telheiras', 'Cais do Sodré', 78, [750, 20])
 
 green = [telheiras, campo_grande, alvalade, roma, areeiro, alameda, arroios, anjos, intendente, martim_moniz, rossio, baixa_chiado, cais_do_sodre]
@@ -41,22 +41,22 @@ yellow = [odivelas, senhor_roubado, ameixoeira, lumiar, quinta_conchas, campo_gr
 
 #vermelha
 aeroporto = Station('Aeroporto', 'Aeroporto', 'São Sebastião', 0, [20, 300])
-encarnacao = Station('Encarnação', 'Aeroporto', 'São Sebastião', 7, None)
-moscavide = Station('Moscavide', 'Aeroporto', 'São Sebastião', 12, None)
-oriente = Station('Oriente', 'Aeroporto', 'São Sebastião', 20, None)
-cabo_ruivo = Station('Cabo Ruivo', 'Aeroporto', 'São Sebastião', 24, None)
-olivais = Station('Olivais', 'Aeroporto', 'São Sebastião', 29, None)
-chelas = Station('Chelas', 'Aeroporto', 'São Sebastião', 36, None)
-bela_vista = Station('Bela Vista', 'Aeroporto', 'São Sebastião', 41, None)
-olaias = Station('Olaias', 'Aeroporto', 'São Sebastião', 48, None)
+encarnacao = Station('Encarnação', 'Aeroporto', 'São Sebastião', 7, [100, 300])
+moscavide = Station('Moscavide', 'Aeroporto', 'São Sebastião', 12, [180, 300])
+oriente = Station('Oriente', 'Aeroporto', 'São Sebastião', 20, [250, 300])
+cabo_ruivo = Station('Cabo Ruivo', 'Aeroporto', 'São Sebastião', 24, [325, 300])
+olivais = Station('Olivais', 'Aeroporto', 'São Sebastião', 29, [400, 300])
+chelas = Station('Chelas', 'Aeroporto', 'São Sebastião', 36, [500, 300])
+bela_vista = Station('Bela Vista', 'Aeroporto', 'São Sebastião', 41, [600, 300])
+olaias = Station('Olaias', 'Aeroporto', 'São Sebastião', 48, [680, 300])
 alameda = Station('Alameda', 'Aeroporto', 'São Sebastião',52, [750, 300])
 saldanha = Station('Saldanha', 'Aeroporto', 'São Sebastião', 59, [845, 300])
-sao_sebastiao = Station('São Sebastião', 'Aeroporto', 'São Sebastião', 70, [980, 300])
+sao_sebastiao = Station('São Sebastião', 'Aeroporto', 'São Sebastião', 70, [1115, 300])
 
-red = [aeroporto, encarnacao, moscavide, oriente, cabo_ruivo, olivais, chelas, bela_vista, alameda, saldanha, sao_sebastiao]
+red = [aeroporto, encarnacao, moscavide, oriente, cabo_ruivo, olivais, chelas, bela_vista, olaias, alameda, saldanha, sao_sebastiao]
 
 #azul
-amadora_este = Station('Amadora Este', 'Amadora Este', 'Santa Apolónia', 0, [1450, 700])
+amadora_este = Station('Amadora Este', 'Amadora Este', 'Santa Apolónia', 0, [1450, 500])
 alfornelos = Station('Alfornelos', 'Amadora Este', 'Santa Apolónia', 7, None)
 pontinha = Station('Pontinha', 'Amadora Este', 'Santa Apolonia', 16, None)
 carnide = Station('Carnide', 'Amadora Este', 'Santa Apolónia', 21, None)
@@ -65,14 +65,14 @@ alto_moinhos = Station('Alto dos Moinhos', 'Amadora Este', 'Santa Apolónia', 36
 laranjeiras = Station('Laranjeiras', 'Amadora Este', 'Santa Apolónia', 44, None)
 jardim_zoologico = Station('Jardim Zoológico', 'Amadora Este', 'Santa Apolónia', 49, None)
 praca_espanha = Station('Praça Espanha', 'Amadora Este', 'Santa Apolónia', 58, None)
-sao_sebastiao = Station('São Sebastião', 'Amadora Este', 'Santa Apolónia', 68, [980, 300])
+sao_sebastiao = Station('São Sebastião', 'Amadora Este', 'Santa Apolónia', 68, None)
 parque = Station('Parque', 'Amadora Este', 'Santa Apolónia', 72, None)
-marques_pombal = Station('Marquês de Pombal', 'Amadora Este', 'Santa Apolónia', 77, [885,220])
+marques_pombal = Station('Marquês de Pombal', 'Amadora Este', 'Santa Apolónia', 77, [905,175])
 avenida = Station('Avenida', 'Amadora Este', 'Santa Apolónia', 79, None)
 restauradores = Station('Restauradores', 'Amadora Este', 'Santa Apolónia', 82, None)
 baixa_chiado = Station('Baixa Chiado', 'Amadora Este', 'Santa Apolónia', 87, None)
 terreio_paço = Station('Terreiro Paço', 'Amadora Este', 'Santa Apolónia', 91, None)
-santa_apolonia = Station('Amadora Este', 'Amadora Este', 'Santa Apolónia', 98, [650, 20])
+santa_apolonia = Station('Santa Apolónia', 'Amadora Este', 'Santa Apolónia', 98, [650, 20])
 
 blue = [amadora_este, alfornelos, pontinha, carnide, colegio_militar, alto_moinhos, laranjeiras, jardim_zoologico, praca_espanha, sao_sebastiao, parque, marques_pombal, avenida, restauradores, baixa_chiado, terreio_paço, santa_apolonia]
 
@@ -84,9 +84,9 @@ init_pos = {
 }
 
 end_pos = {
-    'red': [980, 300],
-    'green': [750, 650],
-    'blue': [1450, 700],
+    'red': [1115, 300],
+    'green': [750, 600],
+    'blue': [1450, 500],
     'yellow': [980, 20]
 }
 
