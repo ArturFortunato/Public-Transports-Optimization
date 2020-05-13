c4816929eb7a9a020808b3df22ee06f15dc88a9d
JMVCoelho <joao.vares.coelho@tecnico.ulisboa.pt>
2020-05-13 19:31:07 +0100

    time series forecasting (ARIMA) in a random dataset. TODO: get real dataset to estimate the model parameters (and check if the model is appropriate for such dataset

---
+++ src/Forecasting.py
@@ -0,0 +1,45 @@
+import numpy
+from pandas import read_csv
+from pandas import DataFrame
+from statsmodels.tsa.arima_model import ARIMA
+from matplotlib import pyplot
+import datetime
+import random
+
+#generating a random dataset
+#import the normal datetime for this, not panda's
+""" init = datetime.datetime(2020, 12 , 1, 6, 15)
+with open("random_dataset.csv", "w") as f:
+    f.write("time,number\n")
+    while True:
+        f.write(str(init) + "," + str(random.randint(10,50)) + "\n")
+        init = init + datetime.timedelta(minutes=15) 
+        if init.day == 31:
+            break  """
+
+series = read_csv('random_dataset.csv', header=0, parse_dates=[0], index_col=0, squeeze=True)
+
+model = ARIMA(series, order=(5,1,0)) #check the values for the model with the real dataset
+model_fit = model.fit(disp=0)
+
+
+#predict from x to y. x has to be in-sample timestamp
+output1 = model_fit.predict("2020-12-30 23:45:00", "2020-12-31 11:15:00")
+#forecast the 1st out of sample result
+output2 = model_fit.forecast()
+
+
+
+#also use this to check if values are ok
+""" print(model_fit.summary())
+# plot residual errors
+residuals = DataFrame(model_fit.resid)
+residuals.plot()
+pyplot.show()
+residuals.plot(kind='kde')
+pyplot.show()
+print(residuals.describe()) """
+
+#prediction
+
+    
