abd31e631250a94818eb7ddef1d959c72e0dbe68
JMVCoelho <joao.vares.coelho@tecnico.ulisboa.pt>
2020-05-17 23:18:22 +0100

    should've gone with last commit

--- data/cleaner.py
+++ data/cleaner.py
@@ -1,12 +1,13 @@
+# - coding: utf-8 --
 import pandas as pd
 
-#Create datasets for estimating number of people entering a station at a given time
+#Create datasets for estimating number of people entering d station at d given time
 
 file = "parsed_data/full_data/samplemetro_only_entries_station_names.csv"
 endfolder1 = "parsed_data/data_per_station_count/"
 endfolder2 = "parsed_data/data_per_station_exits/"
 
-stations =['aeroporto', 'alameda', 'alfornelos', 'alto dos moinhos', 'alvalade', 'amadora este', 'ameixoeira', 'anjos', 'areeiro', 'avenida', 'baixa chiado', 'bela vista', 'cabo ruivo', 'cais do sodr�', 'campo grande', 'campo pequeno', 'carnide', 'chelas', 'cidade universit�ria', 'col�gio militar', 'encarna��o', 'entre campos', 'intendente', 'jardim zool�gico', 'laranjeiras', 'lumiar', 'marqu�s de pombal', 'martim moniz', 'moscavide', 'odivelas', 'olaias', 'olivais', 'oriente', 'parque', 'picoas', 'pontinha', 'pra�a de espanha', 'quinta das conchas', 'rato', 'reboleira', 'restauradores', 'roma', 'rossio', 'saldanha', 'santa apol�nia', 's�o sebasti�o', 'senhor roubado', 'telheiras', 'terreiro pa�o']
+stations = ['aeroporto', 'alameda', 'alfornelos', 'alto dos moinhos', 'alvalade', 'amadora este', 'ameixoeira', 'anjos', 'areeiro', 'avenida', 'baixa chiado', 'bela vista', 'cabo ruivo', 'cais do sodré', 'campo grande', 'campo pequeno', 'carnide', 'chelas', 'cidade universitária', 'colégio militar', 'encarnaçao', 'entre campos', 'intendente', 'jardim zoológico', 'laranjeiras', 'lumiar', 'marquês de pombal', 'martim moniz', 'moscavide', 'odivelas', 'olaias', 'olivais', 'oriente', 'parque', 'picoas', 'pontinha', 'praça de espanha', 'quinta das conchas', 'rato', 'reboleira', 'restauradores', 'roma', 'rossio', 'saldanha', 'santa apolónia', 'sao sebastiao', 'senhor roubado', 'telheiras', 'terreiro paço']
 
 
 def generate_count_per_station_per_hour():
@@ -16,12 +17,16 @@ def generate_count_per_station_per_hour():
     del csv['linha_entrada']
     del csv['linha_saida']
     del csv['estacao_saida'] 
+
+    #hack
+    add = "2018-10-01 00:00:00,0\n2018-11-01 23:45:00,0\n"
     
     for station in stations:
         reduced = csv[csv['estacao_entrada'] == station]
         summed = reduced.groupby('date')['count'].sum()
-        summed.to_csv(endfolder1 + station + ".csv", index=True, sep=',')    
-        
+        summed.to_csv(endfolder1 + station + ".csv", index=True, sep=',')
+        with open(endfolder1 + station + ".csv", 'd' , encoding = "latin1") as f:
+            f.write(add)
     return "done"
 
 def generate_exits_per_station_per_hour():
